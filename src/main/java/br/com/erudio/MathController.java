2dcb502b825a73e9665879d4d981fa52170a4c12
Samuel <samuel.estudo778@gmail.com>
2023-02-21 14:29:56 -0300

    feat: add all Math properties

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -13,31 +13,87 @@ public class MathController {
             method = RequestMethod.GET)
 
     public double sum(@PathVariable(value = "numberOne") String numberOne,
-                      @PathVariable(value = "numberTwo") String numberTwo) throws  Exception{
+                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}",
+            method = RequestMethod.GET)
+    public double subtraction(@PathVariable(value = "numberOne") String numberOne,
+                              @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}",
+            method = RequestMethod.GET)
+    public double multiplication(@PathVariable(value = "numberOne") String numberOne,
+                              @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}",
+            method = RequestMethod.GET)
+    public double division(@PathVariable(value = "numberOne") String numberOne,
+                                 @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+    }
+
+    @RequestMapping(value = "/mean/{numberOne}/{numberTwo}",
+            method = RequestMethod.GET)
+
+    public double mean(@PathVariable(value = "numberOne") String numberOne,
+                      @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return (convertToDouble(numberOne) + convertToDouble(numberTwo)) /2 ;
+    }
+
+    @RequestMapping(value = "/sqrt/{number}",
+            method = RequestMethod.GET)
+    public double sqrt(@PathVariable(value = "number") String number) throws Exception {
+
+        if (!isNumeric(number)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        
+        return Math.sqrt(convertToDouble(number));
+    }
+
     private double convertToDouble(String strNumber) {
-        if(strNumber == null ){
+        if (strNumber == null) {
             return 0D;
         }
-        String number = strNumber.replaceAll(",",".");
+        String number = strNumber.replaceAll(",", ".");
 
-        if(isNumeric(number)){
+        if (isNumeric(number)) {
             return Double.parseDouble(number);
         }
         return 0D;
     }
 
     private boolean isNumeric(String strNumber) {
-        if(strNumber == null ){
+        if (strNumber == null) {
             return false;
         }
-        String number = strNumber.replaceAll(",",".");
+        String number = strNumber.replaceAll(",", ".");
 
         return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
